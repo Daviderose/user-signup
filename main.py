6ef20ce081859a3e9b8a53f36cab7806ff88c515
David Rose <fullbloomdrose@yahoo.com>
2018-03-21 14:29:09 -0500

    finished validation logic, added some CSS

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, redirect, render_template
-import os
+from flask import Flask, request, render_template
+import re
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -18,48 +18,58 @@ def validate():
     email = request.form['email']
     
     # define error variables
-    empty_field_error = ''
-    invalid_chars_error = ''
-    passwords_dont_match_error = ''
+    invalid_user_error = 'Please enter a valid username'
+    invalid_password_error = 'Please enter a valid password'
+    passwords_dont_match_error = 'Passwords do not match'
     invalid_email_error = ''
     
-    # check for empty fields
-    check_list = [user,password,password2]
-    for field in check_list:
-        if len(field) < 1:
-            empty_field_error = 'Please fill out field'
-            
-    # check character length 3-20
-    for field in check_list:
-        if len(field) < 3 or len(field)> 20:
-            invalid_chars_error = 'Please enter phrase no less than 3 and no greater than 20'
+    # check username and password
+    while len(user) > 2 and len(user) < 21:
+        if re.search(' ', user):
+            break
+        invalid_user_error = ''
+        break
 
-    # check passwords match
-    while password != password2:
-        passwords_dont_match_error = 'Passwords do not match'
-        password = ''
-        password2 = ''
+    while len(password) > 2 and len(password) < 21:
+        if re.search(' ', password):
+            break
+        invalid_password_error = ''
+        break
+
+    # check if passwords match
+    if password == password2 and len(password) > 0:
+        passwords_dont_match_error = ''
         
     # check valid email
     while email != '':
+        if len(email) < 3:
+            invalid_email_error = 'Your email must be longer than 3 characters.'
+            break
+        if len(email) > 20:
+            invalid_email_error = 'Your email cannot be longer than 20 characters'
+            break
+        if re.search(' ', email):
+            invalid_email_error = 'Your email cannot contain spaces'
+            break
         at_sign = 0
         period = 0
         for char in email:
             if char == '@':
                 at_sign += 1
-            if char == '.':
+            elif char == '.':
                 period += 1
         
         if at_sign == 1 and period == 1:
             invalid_email_error = ''
         else:
-            invalid_email_error = 'Please enter a valid email address'
+            invalid_email_error = 'Your email must contain only one "@" and one "."'
+        break
 
-    if not empty_field_error and not invalid_chars_error and not passwords_dont_match_error and not invalid_email_error:
+    if not invalid_user_error and not invalid_password_error and not passwords_dont_match_error and not invalid_email_error:
         return render_template('welcome_page.html', title="Welcome Page", name=user)
     else:
-        return render_template('user_signup.html', name=user, empty_field_error=empty_field_error, 
-        invalid_chars_error=invalid_chars_error,
+        return render_template('user_signup.html', name=user, email=email, invalid_user_error=invalid_user_error, 
+        invalid_password_error=invalid_password_error,
         passwords_dont_match_error=passwords_dont_match_error,
         invalid_email_error=invalid_email_error)
 
